d6087a0004f5ea6de79209725557c9925bf3d3d0
MakJhonny <makykamacho@gmail.com>
2021-09-08 20:37:02 -0400

    dbconfig fixed

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "postgres",
-  DB: "auth",
+  HOST: "ec2-54-173-138-144.compute-1.amazonaws.com",
+  PORT: 5432,
+  USER: "hgpmlfhmjxvnfr",
+  PASSWORD: "e3fcf341e4ff4a68075b951e1c9a75239afaa42d7eccc3e9c7db81bda6c77a05",
+  DB: "d966qfatdj765h",
   dialect: "postgres",
+  ssl: true, 
+  dialectOptions: {
+    ssl: true
+  },
   pool: {
-    max: 5,
+    max: 10,
     min: 0,
     acquire: 30000,
     idle: 10000
