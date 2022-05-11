9540fdda9697ae2fc0a1e992baf2c4a5fcb7b69e
Jacovzap <jacovzap@gmail.com>
2022-05-11 00:42:47 -0400

    s2

--- server.js
+++ server.js
@@ -15,23 +15,3 @@ const PORT = process.env.PORT || localhostPort;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-
-
-
-console.log("AAAAAAAAAAAAAAAAAAAAAAAAAA")
-console.log("AAAAAAAAAAAAAAAAAAAAAAAAAA")
-console.log("AAAAAAAAAAAAAAAAAAAAAAAAAA")
-console.log("AAAAAAAAAAAAAAAAAAAAAAAAAA")
-
-console.log(process.env.AUTH_SECRET)
-console.log(process.env.DB_HOST)
-console.log(process.env.DB_PORT)
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASSWORD)
-console.log(process.env.DB_DIALECT)
-console.log(process.env.EMAIL_PORT)
-console.log(process.env.EMAIL_SERVICE)
-console.log(process.env.EMAIL_AUTH_USER)
-console.log(process.env.EMAIL_AUTH_PASS)
-console.log(process.env.URL_CAMBIO)
