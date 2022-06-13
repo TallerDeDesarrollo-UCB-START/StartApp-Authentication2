d90cd80de1e5a79eb71b662391f3db708ceb944c
Brian-Almendras-SC <brian.almendras@ucb.edu.bo>
2022-06-13 17:37:31 -0400

    Fix smtp config and recovery account

--- app/config/email.config.js
+++ app/config/email.config.js
@@ -3,18 +3,22 @@ var smtpTransport = require('nodemailer-smtp-transport');
 require("dotenv").config();
 
 exports.transporter = nodemailer.createTransport({
-  secure:true,
+  host: process.env.EMAIL_SERVICE_HOST, // hostname
   port:process.env.EMAIL_PORT,
-  service: process.env.EMAIL_SERVICE,
+  secureConnection: false,
+  tls: {
+     ciphers:'SSLv3'
+  },
   auth: {
     user: process.env.EMAIL_AUTH_USER,
     pass: process.env.EMAIL_AUTH_PASS,
   },
+
   debug: true,
   logger: true
 });
 
 exports.mailOptions = {
-  from: "ucb.start.project@gmail.com",
+  from: "StartAmericasTogheter2022@hotmail.com",
   subject: "Bienvenid@ a la comunidad de START Americas Together!",
 };
